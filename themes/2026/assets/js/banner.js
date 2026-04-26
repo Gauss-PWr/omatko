68b6e458d3bbdeedaa1a9c4fe7969e9df43ee629
Nikodem Drelak <nikodemd332@gmail.com>
2026-04-26 13:56:07 +0200

    countdown timer

--- themes/2026/assets/js/banner.js
+++ themes/2026/assets/js/banner.js
@@ -1,4 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
   const targetDate = new Date(2026, 11, 5, 13, 30, 0);
-  countdownToDate(targetDate, "timer", "Konferencja rozpoczęta!");
+  countdownToDate(targetDate, "timer", "Wystartowaliśmy!");
+
+  const timerLabel = document.getElementById("timer-label");
+  const timeRemaining = targetDate.getTime() - new Date().getTime();
+  if (timerLabel) {
+    if (timeRemaining <= 0) {
+      timerLabel.style.display = "none";
+    } else {
+      setTimeout(() => {
+        timerLabel.style.display = "none";
+      }, timeRemaining);
+    }
+  }
 });
